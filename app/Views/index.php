71107189d39cfe866c3fd4856727643b38a75f5b
David042 <Davidsarka242@gmail.com>
2021-06-03 01:45:09 -0700

    upload

---
+++ app/Views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Cloning Uniabuja Page</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css');?>">
+</head>
+<body>
+  <h1>Cloned Unibuja</h1>
+
+
+</body>
+</html>
