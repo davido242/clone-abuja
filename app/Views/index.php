2aaebb6e46a51f56fb0c310b2ab0ba771deae00c
David042 <Davidsarka242@gmail.com>
2021-06-09 14:28:29 -0700

    pure clonning biginning

--- app/Views/index.php
+++ app/Views/index.php
@@ -2,37 +2,29 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Cloning Uniabuja Page</title>
+  <title>Cloning Uniabuja Portal</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="icon"  href="company-logo.jpg">
+  <!-- font Awesome CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css');?>">
 </head>
 <body>
-  <nav>
-    <img src="<?php echo base_url('images/npower.png');?>">
-    <h1>nasims (npower) biometric client</h1>
-  </nav>
-  <div class="wrap-main">
-    <table>
-      <tr>
-        <td class="n-info">Module:</td>
-        <td>Nasims Biometric</td>
-      </tr>
-      <tr>
-        <td class="n-info">Package Name:</td>
-        <td>Nasims Biometric Enrollment</td>
-      </tr>
-      <tr>
-        <td class="n-info">Publisher:</td>
-        <td>Nasims (Npower)</td>
-      </tr>
-    </table>
-    <p>This prerequisite  below is required and should be installed seperately before clicking the install button:</p>
-    <ol>
-      <li>Biomenbfjabfjbajfbafj afjsjdabfjdbadbjfbsjfa fdafjk</li>
-      <li>Biomenbfjabfjbajfbafj afjsjdabfjdbadbjfbsjfa fdafjk</li>
-    </ol>
-    <input type="submit" value="Install">
+  <div class="image-container">
+    <img src="https://portal.uniabuja.edu.ng/assets/images/banner.png">
   </div>
-
+  <div class="form-container">
+    <form class="main-form" action="#" method="GET">
+      <label class="lab1">Please sign in to your account below.</label>
+      <input type="text" name="username" max="12" min="5" required>
+      <input type="password" name="password" max="10" min="8" required>
+      <label class="lab1">New Application? <a href="https://portal.uniabuja.edu.ng/app_guide.php">Click here to Apply</a></label>
+      <button> <a href="#">Recover Application No.</a></button>
+      <button> <a href="#">Recover Password</a></button>
+      <button> <a href="#">Login to Dashboard</a></button>
+    </form>
+  </div>
 
 <script src="<?php echo base_url('js/main.js');?>"></script>
 </body>
